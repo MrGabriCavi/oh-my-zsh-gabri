292217df92e34a8736f1ee5ee02a5a94c27c8296
Gabriele C <gabri.cavi@gmail.com>
2022-10-20 11:59:37 +0000

    Added my public ip command

--- setup-zshrc.zsh
+++ setup-zshrc.zsh
@@ -4,3 +4,8 @@ export HEADLINE_DO_CLOCK='true'
 
 alias reload.zsh="source ~/.zshrc"
 alias my-rsync="rsync -auvzr --progress"
+alias ssh--ubu-sarge="ssh gabri@ubu-sarge"
+alias ssh--ubu-rush="ssh gabri@ubu-rush"
+alias my-ip="curl https://checkip.amazonaws.com"
+
+
