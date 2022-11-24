791f14fd320bfa7d7829548974e16f584e2257f6
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2022-11-24 17:28:24 +0100

    Added auto mount with disk wale up

--- setup-zshrc.zsh
+++ setup-zshrc.zsh
@@ -4,9 +4,7 @@ export HEADLINE_DO_CLOCK='true'
 
 alias reload.zsh="source ~/.zshrc"
 alias my-rsync="rsync -auvzr --progress"
-alias ssh--ubu-sarge="ssh gabri@ubu-sarge"
-alias ssh--ubu-rush="ssh gabri@ubu-rush"
 alias my-ip="curl https://checkip.amazonaws.com"
 alias cls="clear"
-
-
+alias ssh--ubu-sarge="ssh gabri@ubu-sarge"
+alias ssh--ubu-rush="ssh gabri@ubu-rush"
